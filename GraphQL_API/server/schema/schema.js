b7603a164e597c12fe202e42dc23b15d530eefda
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2024-03-05 22:44:12 +0000

    finally got task 0-2 working

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,4 +1,10 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLScalarType } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema} = require('graphql');
+const _ = require('lodash');
+
+const tasks = [
+  { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)' },
+  { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order' }
+];
 
 const TaskType = new GraphQLObjectType({
     name: 'Task',
@@ -17,7 +23,7 @@ const TaskType = new GraphQLObjectType({
         type: TaskType,
         args: { id: { type: GraphQLString } },
         resolve(parent, args) {
-            return Project.findById(id);
+          return _.find(tasks, { id: args.id });
         }
       }
     }
@@ -26,3 +32,4 @@ const TaskType = new GraphQLObjectType({
   module.exports = new GraphQLSchema({
     query: RootQuery
   });
+  
