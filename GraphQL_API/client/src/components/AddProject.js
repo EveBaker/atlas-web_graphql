1ac1bcce372fd6ae59ec45c8bacf1cdac94978fd
Eve Baker <Evebaker0162@gmail.com>
2024-03-20 17:04:29 -0500

    task 12

--- GraphQL_API/client/src/components/AddProject.js
+++ GraphQL_API/client/src/components/AddProject.js
@@ -1,74 +1,54 @@
-import {
-  useState,
-  //useEffect
-} from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { addProjectMutation } from "../queries/queries"; // Adjust the import path as necessary
 
-
-function AddProject(props) {
-
+function AddProject() {
   const [inputsProject, setInputsProject] = useState({
     title: '',
     weight: 1,
     description: ''
-
   });
 
-  const handleChange = (e) => {
-    const newInputsProject = {
-      ...inputsProject
-    };
-    if (e.target.name === "weight") newInputsProject[e.target.name] = parseInt(e.target.value)
-    else newInputsProject[e.target.name] = e.target.value
-    setInputsProject(newInputsProject)
-  }
+  const [addProject, { data, loading, error }] = useMutation(addProjectMutation);
 
-  return ( <
-    form class = "project"
-    id = "add-project"
-    /*onSubmit = {...}*/ >
-    <
-    div className = "field" >
-    <
-    label > Project title: < /label> <
-    input type = "text"
-    name = "title"
-    onChange = {
-      handleChange
+  const handleChange = (e) => {
+    const newInputsProject = { ...inputsProject };
+    newInputsProject[e.target.name] = e.target.name === "weight" ? parseInt(e.target.value, 10) : e.target.value;
+    setInputsProject(newInputsProject);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await addProject({
+        variables: {
+          title: inputsProject.title,
+          weight: inputsProject.weight,
+          description: inputsProject.description,
+        },
+      });
+      console.log("Added project", data);
+    } catch (error) {
+      console.error("Error adding project", error);
     }
-    value = {
-      inputsProject.title
-    }
-    / > < /
-    div > <
-    div className = "field" >
-    <
-    label > Weight: < /label> <
-    input type = "number"
-    name = "weight"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputsProject.weight
-    }
-    / > < /
-    div >
-    <
-    div className = "field" >
-    <
-    label > description: < /label> <
-    textarea name = "description"
-    onChange = {
-      handleChange
-    }
-    value = {
-      inputsProject.description
-    }
-    / > < /
-    div >
-    <
-    button > + < /button> < /
-    form >
+  };
+
+  return (
+    <form className="project" id="add-project" onSubmit={handleSubmit}>
+      <div className="field">
+        <label>Project title:</label>
+        <input type="text" name="title" onChange={handleChange} value={inputsProject.title} />
+      </div>
+      <div className="field">
+        <label>Weight:</label>
+        <input type="number" name="weight" onChange={handleChange} value={inputsProject.weight} />
+      </div>
+      <div className="field">
+        <label>Description:</label>
+        <textarea name="description" onChange={handleChange} value={inputsProject.description}></textarea>
+      </div>
+      <button type="submit">Add Project</button>
+    </form>
   );
 }
 
